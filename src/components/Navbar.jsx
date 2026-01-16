d3db479073a843e695fdd387bc5c767b3b581b69
manish sharma <msmanish407@gmail.com>
2026-01-16 22:30:35 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import dayjs from "dayjs";
+
+import {navIcons, navLinks} from "#constants"
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+
+            <img src="/images/logo.svg" alt="apple-logo-navbar"/>
+            <p className="font-bold">Manish's Portfolio</p>
+
+                <ul>
+                    {navLinks.map(({id, name}) => (
+
+                        <li key={id}>
+                             <p>{name}</p>
+                        </li>
+                    ))}
+                </ul>
+
+            </div>
+
+<div>
+    <ul>
+        {navIcons.map(({id, img}) => (
+            <li key={id}>
+                <img src={img} className="icon-hover" alt={`icon-${id}`}/>
+            </li>
+        ))}
+    </ul>
+</div>
+        </nav>
+    )
+}
+export default Navbar
